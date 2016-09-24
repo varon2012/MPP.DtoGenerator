8d7b37b9d3443150d01ffe5985d1988538276402
Yury Rybak <yuryrybak97@gmail.com>
2016-09-25 01:59:14 +0300

    [DtoGenerator.Generator] Added basic data types

---
+++ DtoGenerator/DtoGenerator.Generator/DtoClassDeclaration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DtoGenerator.Generator
+{
+    public class DtoClassDeclaration
+    {
+        public string ClassName { get; }
+        public IEnumerable<DtoClassProperty> Properties { get; }
+
+        public DtoClassDeclaration(string className, IEnumerable<DtoClassProperty> properties)
+        {
+            if (className == null) throw new ArgumentNullException(nameof(className));
+            if (properties == null) throw new ArgumentNullException(nameof(properties));
+
+            ClassName = className;
+            Properties = properties;
+        }
+    }
+}
---
+++ DtoGenerator/DtoGenerator.Generator/DtoClassProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DtoGenerator.Generator
+{
+    public class DtoClassProperty
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public string Format { get; }
+
+        public DtoClassProperty(string name, string type, string format)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (type == null) throw new ArgumentNullException(nameof(type));
+            // type format can be null
+
+            Name = name;
+            Type = type;
+            Format = format;
+        }
+    }
+}
