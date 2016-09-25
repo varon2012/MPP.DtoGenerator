2dc5a1cc81bdcd6ddc313fc80ce187c7aedadaf1
Dmitry <mr.blacky01@gmail.com>
2016-09-25 19:09:29 +0300

    + Add class that contains list of classDescriptions of result DTO classes

---
+++ DtoGeneratorProgram/DtoGeneratorProgram/Descriptors/DescriptionsOfClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoGeneratorProgram.Descriptors
+{
+    internal class DescriptionsOfClass
+    {
+        public List<ClassDescriptor> classDescriptions { get; set; }
+
+        public DescriptionsOfClass()
+        {
+            classDescriptions = new List<ClassDescriptor>();
+        }
+    }
+}
