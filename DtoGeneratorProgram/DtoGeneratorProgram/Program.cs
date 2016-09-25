eec51b521206ad52a25f062d8701993b84e0203e
Dmitry <mr.blacky01@gmail.com>
2016-09-26 01:24:20 +0300

    + Add more functionality to work with DTO generator(including output classes, reading from configuration file)

--- DtoGeneratorProgram/DtoGeneratorProgram/Program.cs
+++ DtoGeneratorProgram/DtoGeneratorProgram/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Script.Serialization;
-using DtoGeneratorProgram.Descriptors;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+
+using DtoGenerator;
+using DtoGenerator.Descriptors;
 
 namespace DtoGeneratorProgram
 {
@@ -14,17 +15,45 @@ namespace DtoGeneratorProgram
         static void Main(string[] args)
         {
             string path = "samples.json";
+            string taskCountPath = "taskCount.txt";
+        
          
-            if (File.Exists(path))
+            if ( (File.Exists(path)) && (File.Exists(taskCountPath)) )
             {
                 string json = File.ReadAllText(path);
+                int countOfThreads = Int32.Parse(File.ReadAllText(taskCountPath));
+
                 JavaScriptSerializer ser = new JavaScriptSerializer();
                 DescriptionsOfClass classes = ser.Deserialize<DescriptionsOfClass>(json);
                 Console.WriteLine(classes.classDescriptions.Count);
+
+                DTOGenerator tempDto = new DTOGenerator(classes, new List<TypeDescription.TypeDescriptor>(), countOfThreads);
+                Dictionary<string, CodeCompileUnit> temp = tempDto.GetUnitsOfDtoClasses();
+
+                foreach(var unit in temp)
+                {
+                    SaveCode(unit.Key, unit.Value);
+                }
             }
 
-
+            
             Console.ReadLine();
+        }
+
+        public static void SaveCode(string className, CodeCompileUnit compileunit)
+        {
+            // Build the source file name with the appropriate
+            // language extension.
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            String sourceFile = className + ".cs";
+
+            // Create an IndentedTextWriter, constructed with
+            // a StreamWriter to the source file.
+            IndentedTextWriter codeWriter = new IndentedTextWriter(new StreamWriter(sourceFile, false), "    ");
+            // Generate source code using the code generator.
+            provider.GenerateCodeFromCompileUnit(compileunit, codeWriter, new CodeGeneratorOptions());
+            // Close the output file.
+            codeWriter.Close();
         }
     }
 }
