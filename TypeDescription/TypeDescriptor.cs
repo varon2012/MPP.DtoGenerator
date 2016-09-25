afdf8b7fe7790b6de3f667a3dd2376d5d7dba6e0
Dmitry <mr.blacky01@gmail.com>
2016-09-25 20:15:46 +0300

    - Delete unused derective

--- TypeDescription/TypeDescriptor.cs
+++ TypeDescription/TypeDescriptor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TypeDescription
+﻿namespace TypeDescription
 {
     public abstract class TypeDescriptor
     {
