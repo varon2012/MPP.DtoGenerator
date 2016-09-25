8bdc9a411524df35eaf8052b7520f632f2385004
Dmitry <mr.blacky01@gmail.com>
2016-09-26 00:48:47 +0300

    +Add generation of Unit of DTO class and try to push thme to threadpool

--- DtoGenerator/DTOGenerator.cs
+++ DtoGenerator/DTOGenerator.cs
@@ -7,8 +7,9 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Collections.Generic;
+using System.Threading;
 
-using DtoGeneratorProgram.Descriptors;
+using DtoGenerator.Descriptors;
 using TypeDescription;
 
 namespace DtoGenerator
@@ -19,6 +20,12 @@ namespace DtoGenerator
         private List<TypeDescription.TypeDescriptor> Types;
         private int CountOfTasks;
 
+        public struct ThreadConrtex
+        {
+            public Dictionary<string, CodeCompileUnit> result;
+            public ClassDescriptor classDescription;
+        }
+
         public DTOGenerator(DescriptionsOfClass classes, List<TypeDescription.TypeDescriptor> types,int tasksCount)
         {
             Classes = classes;
@@ -26,20 +33,58 @@ namespace DtoGenerator
             CountOfTasks = tasksCount; 
         }
 
-        public List<CodeCompileUnit> GetUnitsOfDtoClasses()
+        public Dictionary<string,CodeCompileUnit> GetUnitsOfDtoClasses()
         {
+            Dictionary<string, CodeCompileUnit> result = new Dictionary<string, CodeCompileUnit>();
+            ThreadPool.SetMaxThreads(CountOfTasks, Int32.MaxValue);
+            foreach (var dtoClass in Classes.classDescriptions)
+            {
 
-            return new List<CodeCompileUnit>();
+                //ThreadPool.QueueUserWorkItem
+                ThreadConrtex tempContext = new ThreadConrtex();
+                tempContext.result = result;
+                tempContext.classDescription = dtoClass;
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadPoolCallback),new ThreadConrtex());
+            }
+
+            return result;
+        }
+
+        private void ThreadPoolCallback(Object threadContext)
+        {
+            ThreadConrtex data = (ThreadConrtex)threadContext;
+            Dictionary<string, CodeCompileUnit> result = (Dictionary<string, CodeCompileUnit>)data.result;
+            result.Add(data.classDescription.className, GenerateCode(data.classDescription));
+            
         }
         private CodeCompileUnit GenerateCode(ClassDescriptor classDescription)
         {
             CodeCompileUnit compileUnit = new CodeCompileUnit();
-            CodeTypeDeclaration className = new CodeTypeDeclaration(classDescription.className);
-            // Add the new type to the namespace type collection.
-            
-            
 
-            return new CodeCompileUnit();
+            // Declare a new namespace called Samples.
+            CodeNamespace classNameSpace = new CodeNamespace(classDescription.className+"Namespace");
+            // Add the new namespace to the compile unit.
+            compileUnit.Namespaces.Add(classNameSpace);
+
+            CodeTypeDeclaration className = new CodeTypeDeclaration(classDescription.className);
+            className.IsClass = true;
+            className.TypeAttributes = System.Reflection.TypeAttributes.Public | System.Reflection.TypeAttributes.Sealed;
+            classNameSpace.Types.Add(className);
+
+            foreach(var property in classDescription.properties)
+            {
+                CodeMemberProperty tempProperty = new CodeMemberProperty();
+                tempProperty.Attributes = MemberAttributes.Public | MemberAttributes.Final;
+                tempProperty.Type = new CodeTypeReference("String");
+                tempProperty.Name = property.name;
+                tempProperty.HasGet = true;
+                tempProperty.HasSet = true;
+                className.Members.Add(tempProperty);
+            }
+
+
+            return compileUnit;
         }
     }
+
 }
