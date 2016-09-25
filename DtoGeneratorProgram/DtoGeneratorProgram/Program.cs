8bea5c5e5b85d6b189d4cb0e5f3c30152d15777f
Dmitry <mr.blacky01@gmail.com>
2016-09-25 19:12:37 +0300

    + Add reading from json document

--- DtoGeneratorProgram/DtoGeneratorProgram/Program.cs
+++ DtoGeneratorProgram/DtoGeneratorProgram/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using DtoGeneratorProgram.Descriptors;
 
 namespace DtoGeneratorProgram
 {
@@ -10,6 +13,18 @@ namespace DtoGeneratorProgram
     {
         static void Main(string[] args)
         {
+            string path = "samples.json";
+         
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                JavaScriptSerializer ser = new JavaScriptSerializer();
+                DescriptionsOfClass classes = ser.Deserialize<DescriptionsOfClass>(json);
+                Console.WriteLine(classes.classDescriptions.Count);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
