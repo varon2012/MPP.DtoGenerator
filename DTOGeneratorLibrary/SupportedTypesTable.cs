a82092b2d9c45f35acd4a90ba32f6b51c96b0cee
Ivan <vana.shimko@gmail.com>
2016-09-26 23:38:51 +0300

    [DTOGeneratorLibrary]: Add contracts for type handling

---
+++ DTOGeneratorLibrary/SupportedTypesTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DTOGeneratorLibrary
+{
+    public enum TypeKind
+    {
+        Integer,
+        Number,
+        Boolean,
+        String,
+        Object
+    }
+
+    internal class SupportedTypesTable
+    {
+        private readonly List<TypeDescription> _registeredTypes = new List<TypeDescription>();
+
+        internal SupportedTypesTable()
+        {
+            RegisterBuiltinTypes();
+        }
+
+        internal Type GetNetType(TypeKind typeKind, string format)
+        {
+            return _registeredTypes.Find(x => (x.TypeKind == typeKind) && (x.FormatName == format)).NetType;
+        }
+
+        private void RegisterBuiltinTypes()
+        {
+            TypeDescription[] builtinTypes =
+            {
+                new TypeDescription(TypeKind.Integer, "int32", typeof(int)),
+                new TypeDescription(TypeKind.Integer, "int64", typeof(long)),
+                new TypeDescription(TypeKind.Number, "float", typeof(float)),
+                new TypeDescription(TypeKind.Number, "double", typeof(double)),  
+                new TypeDescription(TypeKind.String, "byte", typeof(byte)), 
+                new TypeDescription(TypeKind.Boolean, null, typeof(bool)),
+                new TypeDescription(TypeKind.String, "date", typeof(DateTime)),  
+                new TypeDescription(TypeKind.String, "string", typeof(string)), 
+            };
+
+            foreach (TypeDescription typeDescription in builtinTypes)
+            {
+                RegisterType(typeDescription);
+            }
+        }
+
+        private void RegisterType(TypeDescription typeDescription)
+        {
+            _registeredTypes.Add(typeDescription);
+        }
+    }
+}
---
+++ DTOGeneratorLibrary/TypeDescription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DTOGeneratorLibrary
+{
+    public struct TypeDescription
+    {
+        public readonly TypeKind TypeKind;
+        public readonly string FormatName;
+        public readonly Type NetType;
+
+        public TypeDescription(TypeKind typeKind, string formatName, Type netType)
+        {
+            FormatName = formatName;
+            NetType = netType;
+            TypeKind = typeKind;
+        }
+    }
+}
