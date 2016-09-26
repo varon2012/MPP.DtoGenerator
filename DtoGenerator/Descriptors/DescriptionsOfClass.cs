c8366d863a4ef548f5fe985df88f11f09df25f9b
Dmitry <mr.blacky01@gmail.com>
2016-09-26 22:57:43 +0300

    +Add namespace property

--- DtoGenerator/Descriptors/DescriptionsOfClass.cs
+++ DtoGenerator/Descriptors/DescriptionsOfClass.cs
@@ -5,6 +5,7 @@ namespace DtoGenerator.Descriptors
     public class DescriptionsOfClass
     {
         public List<ClassDescriptor> classDescriptions { get; set; }
+        public string Namespace { get; set; }
 
         public DescriptionsOfClass()
         {
