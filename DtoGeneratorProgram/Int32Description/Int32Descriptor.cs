2baa4448b2ec2cccd12a1304ed8a687bffad40ce
Dmitry <mr.blacky01@gmail.com>
2016-09-27 01:32:50 +0300

    +Add class child of TypeDescriptor

---
+++ DtoGeneratorProgram/Int32Description/Int32Descriptor.cs
@@ -0,0 +1,14 @@
+﻿using TypeDescription;
+
+namespace Int32Description
+{
+    public class Int32Descriptor : TypeDescriptor
+    {
+        public Int32Descriptor()
+        {
+            Type = "integer";
+            Format = "int32";
+            NETType = "System.Int32";
+        }
+    }
+}
