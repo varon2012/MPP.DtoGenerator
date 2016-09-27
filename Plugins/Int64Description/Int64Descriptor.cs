6a85bfa5679d0af88f8b6822ef0ca5211581c879
Dmitry <mr.blacky01@gmail.com>
2016-09-27 21:16:02 +0300

    + Add int32 and int64 descriptions

---
+++ Plugins/Int64Description/Int64Descriptor.cs
@@ -0,0 +1,14 @@
+﻿using TypeDescription;
+
+namespace Int64Description
+{
+    public class Int64Descriptor : TypeDescriptor
+    {
+        public Int64Descriptor()
+        {
+            Type = "integer";
+            Format = "int64";
+            NETType = "System.Int64";
+        }
+    }
+}
