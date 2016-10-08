7a3050b24dafb606d95a8d8ca0e4e6a3c1f50bdb
Anastasia Paramonova <Anastasia Paramonova>
2016-10-09 02:53:54 +0300

    user input added

---
+++ DtoGenerator/Generator.cs
@@ -0,0 +1,57 @@
+﻿using DtoGenerator.CodeGenerators;
+using DtoGenerator.DtoDescriptor;
+using DtoGenerator.DtoDescriptors;
+using DtoGenerator.Parser;
+using System;
+using System.CodeDom;
+
+namespace DtoGenerator
+{
+    public class Generator
+    {
+        private string classesNamespace;
+
+        public Generator(string classesNamespace)
+        {
+            this.classesNamespace = classesNamespace;
+        }
+
+
+        public GeneratedClasses GenerateDtoClasses(string jsonString)
+        {
+            
+            if(jsonString == null)
+            {
+                throw new ArgumentNullException(nameof(jsonString));
+            }
+            if (classesNamespace == null)
+            {
+                throw new ArgumentNullException(nameof(classesNamespace));
+            }
+
+            ClassList list = ParseJsonString(jsonString);
+            GeneratedClasses classes = GenerateClassesCode(list);
+
+            return classes;
+        }
+
+        private ClassList ParseJsonString(string jsonString)
+        {
+            IParser<ClassList> parser = new ClassParser();
+            return parser.Parse(jsonString);
+        }
+
+        private GeneratedClasses GenerateClassesCode(ClassList list)
+        {
+            ICodeGenerator codeGenerator = new CSCodeGenerator();
+            GeneratedClasses classes = new GeneratedClasses();
+            foreach (ClassDescription classDescription in list.classDescriptions)
+            {
+                CodeCompileUnit compileUnit = codeGenerator.GenerateCode(classDescription, classesNamespace);
+                classes.AddClass(compileUnit);
+            }
+
+            return classes;
+        }
+    }
+}
---
+++ DtoGeneratorTest/DtoGeneratorTest/Tester.cs
@@ -0,0 +1,67 @@
+﻿using DtoGenerator;
+using DtoGenerator.CodeGenerators;
+using DtoGeneratorTest.FileIO;
+using DtoGeneratorTest.FileReaders;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace DtoGeneratorTest
+{
+    class Tester
+    {
+        private static string classesNamespace;
+        private static int maxThreadNumber;
+
+        static void Main(string[] args)
+        {
+            if(args.Length < 2)
+            {
+                Console.WriteLine("Wrong number of arguments. You should specify 2 arguments.");
+                Console.WriteLine("1st argument - json file path. 2nd argument - destination directory path.");
+                Console.ReadLine();
+                return;
+            }
+
+            string jsonFilePath = args[0];
+            string directoryPath = args[1];
+
+            classesNamespace = ConfigurationManager.AppSettings["generatedClassesNamespace"];
+            maxThreadNumber = Int32.Parse(ConfigurationManager.AppSettings["maxThreadNumber"]);
+
+            Tester tester = new Tester();
+            tester.test(jsonFilePath, directoryPath);
+            Console.ReadLine();
+        }
+
+        private void test(string jsonFilePath, string directoryPath)
+        {
+            string jsonString = readJsonFile(jsonFilePath);
+            if(jsonString == null)
+            {
+                Console.WriteLine("Incorrect json file path.");
+                return;
+            }
+
+            Generator generator = new Generator(classesNamespace);
+            GeneratedClasses classes = generator.GenerateDtoClasses(jsonString);
+            IFileWriter writer = new CSFileWriter(directoryPath);
+            IEnumerator<CodeCompileUnit> enumerator = classes.GetEnumerator();
+            int i = 0;
+            while (enumerator.MoveNext())
+            {
+                CodeCompileUnit compileUnit = enumerator.Current;
+                writer.Write(compileUnit);
+                i++;
+            }
+            Console.WriteLine("Generated " + i + " classes in directory " + directoryPath);
+        }
+
+        private string readJsonFile(string filePath)
+        {
+            IFileReader reader = new JsonFileReader();
+            return reader.ReadFile(filePath);
+        }
+    }
+}
