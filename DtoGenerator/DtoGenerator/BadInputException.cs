5a87fd95ab8ad9c2615d90b4f5163a5859a03978
danila.ramaniuk <danila.ramaniuk@itechart-group.com>
2016-10-11 14:13:57 +0300

    [DtoGenerator] - Added I/O realization

---
+++ DtoGenerator/DtoGenerator/BadInputException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DtoGenerator
+{
+    [Serializable]
+    internal class BadInputException : Exception
+    {
+        public BadInputException()
+        {
+        }
+
+        public BadInputException(string message) : base(message)
+        {
+        }
+
+        public BadInputException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BadInputException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ DtoGenerator/DtoGenerator/FileWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DtoGenerator
+{
+    internal sealed class FileWriter : IClassWriter
+    {
+        private readonly string _directory;
+
+        public FileWriter(string directory)
+        {
+            if (directory == null) throw new ArgumentNullException(nameof(directory));
+
+            _directory = directory;
+            CreateDirectoryIfNotExists(directory);
+        }
+
+        public void Write(string className, string classCode)
+        {
+            if (className == null) throw new ArgumentNullException(nameof(className));
+            if (classCode == null) throw new ArgumentNullException(nameof(classCode));
+
+            using (var stream = File.OpenWrite($"{_directory}/{className}.cs"))
+            {
+                using (var fileWritter = new StreamWriter(stream, Encoding.UTF8))
+                {
+                    fileWritter.WriteLine(classCode);
+                }
+            }
+        }
+
+        private static void CreateDirectoryIfNotExists(string path)
+        {
+            if (Directory.Exists(path)) return;
+            Directory.CreateDirectory(path);
+        }
+    }
+}
