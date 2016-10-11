cb1aa1dce37c942de2f9bd0349292bea265e737b
danila.ramaniuk <danila.ramaniuk@itechart-group.com>
2016-10-11 17:24:16 +0300

    [DtoGenerator] Added basic logic for application

--- DtoGenerator/DtoGenerator/Program.cs
+++ DtoGenerator/DtoGenerator/Program.cs
@@ -1,9 +1,58 @@
-﻿namespace DtoGenerator
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using DtoGenerator.Generator;
+
+namespace DtoGenerator
 {
     internal sealed class Program
     {
         public static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Usage: <path-to-json-file> <path-to-output-directory>");
+                return;
+            }
+
+            var filename = args[0];
+            var outputPath = args[1];
+
+            try
+            {
+                Process(new JsonParser<DtoClassDescription>(), filename, outputPath);
+            }
+            catch (BadInputException e)
+            {
+                Console.Error.WriteLine($"Error occurred during working with supplied data: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Unknown error occurred: {e.Message}");
+            }
+        }
+
+        private static void Process(IFileParser<DtoClassDescription> parser, string filename, string outputPath)
+        {
+            var classes = parser.Parse(filename);
+
+            // classes generation magic
+            IDictionary<string, string> generatedClasses;
+
+            IClassWriter writter = new FileWriter(outputPath);
+
+            foreach (var className in generatedClasses.Keys)
+            {
+                try
+                {
+                    var classCode = generatedClasses[className];
+                    writter.Write(className, classCode);
+                }
+                catch (IOException e)
+                {
+                    Console.Error.WriteLine($"Error occurred during writing to file: {e.Message}");
+                }
+            }
         }
     }
 }
