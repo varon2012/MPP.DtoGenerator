928304c79581d7446677c9512808dee2051d89a6
Mikhail Snitavets <mksn13@gmail.com>
2016-10-12 16:29:55 +0300

    FromJsonToCsFilesDtoGenerator added, writer added, reader template added

---
+++ FromJsonToCsFilesDtoGenerator/CsFileDtoDeclarationWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DtoGenerator;
+
+namespace FromJsonToCsFilesDtoGenerator
+{
+    public class CsFileDtoDeclarationWriter : IDtoDeclarationWriter
+    {
+        public string OutputDirectoryPath { get; }
+        private bool Disposed { get; set; }
+
+        public CsFileDtoDeclarationWriter(string outputDirectoryPath)
+        {
+            OutputDirectoryPath = outputDirectoryPath;
+            Directory.CreateDirectory(outputDirectoryPath);
+        }
+
+        public void Dispose()
+        {
+            if (!Disposed)
+            {
+                Disposed = true;
+            }
+            else
+            {
+                throw new ObjectDisposedException(ToString());
+            }
+
+        }
+
+        public void Write(DtoDeclaration dtoDeclaration)
+        {
+            string outputFileName = $"{dtoDeclaration.ClassName}.cs";
+            string outputFilePath = Path.Combine(OutputDirectoryPath, outputFileName);
+            File.WriteAllText(outputFilePath, dtoDeclaration.ClassFullText);
+        }
+    }
+}
---
+++ FromJsonToCsFilesDtoGenerator/JsonDtoInfoListReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DtoGenerator;
+
+namespace FromJsonToCsFilesDtoGenerator
+{
+    public class JsonDtoInfoListReader : IDtoInfoListReader
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event Action<DtoInfo> OnDtoInfoRead;
+        public event Action OnReadCompleted;
+
+        public void ReadList()
+        {
+
+        }
+    }
+}
