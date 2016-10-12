d589ac10ad75d7fd065a90f219cc35d82270579c
Mikhail Snitavets <mksn13@gmail.com>
2016-10-13 00:29:16 +0300

    Runner added

---
+++ FromJsonToCsFilesDtoGenerator/Runner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DtoGenerator;
+
+namespace FromJsonToCsFilesDtoGenerator
+{
+    class Runner
+    {
+        private const int ArgumentsCount = 2;
+
+        public static void Main(string[] args)
+        {
+            if (args.Length == ArgumentsCount)
+            {
+                string jsonFilePath = args[0];
+                string outputDirectoryPath = args[1];
+
+                try
+                {
+                    JsonDtoInfoListReader reader = new JsonDtoInfoListReader(jsonFilePath);
+                    CsFileDtoDeclarationWriter writer = new CsFileDtoDeclarationWriter(outputDirectoryPath);
+
+                    DtoGenerator.DtoGenerator generator = new DtoGenerator.DtoGenerator(3, "dtos", reader, writer);
+                    generator.GenerateDtoDeclarations();
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine($"Error: {e.Message}");
+                }
+
+            }
+            else
+            {
+                Console.Error.WriteLine("Error: invalid argument count");
+            }
+        }
+    }
+}
