6d01f0dd38b035ba82617e38fa9f22d1fb4e2a5e
Mikhail Snitavets <mksn13@gmail.com>
2016-10-13 00:38:57 +0300

    Fixed hiding console after execution

--- FromJsonToCsFilesDtoGenerator/Runner.cs
+++ FromJsonToCsFilesDtoGenerator/Runner.cs
@@ -36,6 +36,7 @@ namespace FromJsonToCsFilesDtoGenerator
             {
                 Console.Error.WriteLine("Error: invalid argument count");
             }
+            Console.Read();
         }
     }
 }
